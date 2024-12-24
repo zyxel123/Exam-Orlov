43830af2c0c6437929f3069ba9ce3fffc2444d1c
306 <306@kp15.lan>
2024-12-24 10:07:49 +0300

    Первый коммит добавлены классы и консольное приложение

---
+++ Exam Orlov/DLLibrary/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLibrary
+{
+
+    // Student.cs
+    public class Student
+    {
+        public string Name { get; set; }
+        public double Ocenka { get; set; }
+
+        public Student(string name, double ocenka)
+        {
+            Name = name;
+            Ocenka = ocenka;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"ФИО: {Name}, Средняя оценка: {Ocenka}");
+        }
+    }
+    // Group.cs
+
+
+    public class Group
+    {
+        public string College { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Group(string college)
+        {
+            College = college;
+            Students = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Колледж: {College}");
+        }
+
+        public void ShowAll()
+        {
+            Show();
+            foreach (var student in Students)
+            {
+                student.Show();
+            }
+        }
+    }
+}
---
+++ Exam Orlov/MainProgram Orlov/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLLibrary;
+
+namespace MainProgram_Orlov
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта основного класса Group
+            Group group = new Group("Технический колледж");
+
+            // Создание объектов дополнительного класса Student с произвольными данными
+            Student student1 = new Student("Иванов Иван", 4.5);
+            Student student2 = new Student("Петров Петр", 3.6);
+            Student student3 = new Student("Сидоров Сидор", 4.8);
+            Student student4 = new Student("Кузнецова Анна", 4.2);
+            Student student5 = new Student("Лебедева Ольга", 5.0);
+
+            // Добавление студентов в группу
+            group.AddStudent(student1);
+            group.AddStudent(student2);
+            group.AddStudent(student3);
+            group.AddStudent(student4);
+            group.AddStudent(student5);
+
+            // Сортировка студентов по средней оценке (Ocenka)
+            group.Students.Sort((s1, s2) => s1.Ocenka.CompareTo(s2.Ocenka));
+
+            // Показ всей информации о группе и студентах
+            group.ShowAll();
+
+            // Задержка перед закрытием консоли
+            Console.ReadLine();
+        }
+    }
+}
